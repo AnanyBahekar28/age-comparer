3fdd64b0721496a4b6fa11e21a2753daea5503dd
AnanyBahekar28 <63043517+AnanyBahekar28@users.noreply.github.com>
2020-06-17 15:29:09 +0530

    Add files via upload

---
+++ age_comparing.py
@@ -0,0 +1,19 @@
+age_1 = int(input("Enter the age of user 1: "))
+age_2 = int(input("Enter the age of user 2: "))
+age_3 = int(input("Enter the age of user 3: "))
+
+if age_1 > age_2 and age_1 > age_3:
+    if age_2 > age_3:
+        print("1. User 1\n2. User 2\n3. User 3")
+    else:
+        print("1. User 1\n2. User 3\n3. User 2")
+elif age_2 > age_1 and age_2 > age_3:
+    if age_1 > age_3:
+        print("1, User 2\n2. User 1\n3. User 3")
+    else:
+        print("1. User 2\n2. User 3\n3. User 1")
+else:
+    if age_1 > age_2:
+        print("1, User 3\n2. User 1\n3. User 2")
+    else:
+        print("1. User 3\n2. User 2\n3. User 1")
